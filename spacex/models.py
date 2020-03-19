27d701cffa2c868a323af0d9692ff66b80231b4f
Gabriel <greyesleon33@gmail.com>
2020-03-19 15:20:19 -0400

    Build spacex model

--- spacex/models.py
+++ spacex/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+#models.Model inherits funcionality from django 
+class Spacex(models.Model):
+  titulo = models.CharField(max_length=100)
+  body = models.TextField()
+  fecha = models.CharField(max_length=25)
