73b5efefc8fa298dd745788524e0dc20f9e5c94d
Gabriel <greyesleon33@gmail.com>
2020-03-19 17:18:48 -0400

    changes to a model and db migrations

--- spacex/admin.py
+++ spacex/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here para que aparezcan en el admin.
+
+from .models import SpacexItem
+
+admin.site.register(SpacexItem)
--- spacex/models.py
+++ spacex/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
 #models.Model inherits funcionality from django 
-class Spacex(models.Model):
+class SpacexItem(models.Model):
   titulo = models.CharField(max_length=100)
   body = models.TextField()
   fecha = models.CharField(max_length=25)
+
+  #funcion que define como se vera un modelo en el admin panel
+  def __str__(self):
+      return self.titulo
