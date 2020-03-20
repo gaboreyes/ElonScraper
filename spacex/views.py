976eddfe81c6a83b6ef3e54c9eda87991f1cb28e
Gabriel <greyesleon33@gmail.com>
2020-03-20 01:28:23 -0400

    rendering db info on the first view

--- spacex/views.py
+++ spacex/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
+
+#del directorio actual importa el modelo para poder taerte los objs de la database
+from .models import SpacexItem
 
 # Create your views here.
 
 #funciones que se disparan al visitar una url
 def spacex(request):
-  return render(request, "spacex/spacex.html")
+  #var que almacena todos los objectos de la tabla ordenados por Id
+  items = SpacexItem.objects.all()
+  return render(request, "spacex/spacex.html", {"items": items})
