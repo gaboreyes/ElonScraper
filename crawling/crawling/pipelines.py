7a49ecf56fb28d522a481eaa86bcccfdcc2a285e
Gabriel <greyesleon33@gmail.com>
2020-03-21 12:25:54 -0400

    fix pipeline to build and item obj then store that obj in database

--- crawling/crawling/pipelines.py
+++ crawling/crawling/pipelines.py
@@ -6,7 +6,22 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
+from spacex.models import SpacexItem
+
 class CrawlingPipeline(object):
+
     def process_item(self, item, spider):
-        print("Pipeline: " + item['titulo'][0])
+        #[0] porque siempre vas a estar procesando listas de 1 elemento con cada yield del item
+
+        #print("Pipeline: " + item['titulo'][0])
+        #print("Pipeline: " + item['fecha'][0])
+        #print("Pipeline: " + item['body'][0])
+        #item.save()
+
+        #crea una instancia de SpacexItem con la data guardada en la var item que viene del yield en el spider.py
+        SpacexItem.objects.create(
+            titulo=item['titulo'][0],
+            body=item['body'][0],
+            fecha=item['fecha'][0],
+        )
         return item
